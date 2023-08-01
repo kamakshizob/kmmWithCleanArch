1f66214d69a78a74b7df6ce69fd1a53a83af945e
kamakshitarana <lavitaranatarana@gmail.com>
2023-08-01 11:38:26 +0530

    code with kmm for a movieapp

---
+++ shared/src/commonMain/kotlin/com/dipumba/movies/data/remote/MoviesResponse.kt
@@ -0,0 +1,8 @@
+package com.dipumba.movies.data.remote
+
+//step-2 creating a data class which is containing the data from the movieRemote.
+
+@kotlinx.serialization.Serializable
+internal data class MoviesResponse(
+    val results: List<MovieRemote>
+)
